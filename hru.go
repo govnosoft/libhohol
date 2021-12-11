665d15abf33dd9268543deec35b35049deaf1c11
zemlyak <zemlyak@pandemonica.ru>
2021-12-11 12:57:14 +0300

    Hruuuuu hruuuu

---
+++ hru.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+
+)
+
+func main() {
+	var reader = bufio.NewReader(os.Stdin)
+	total, _ := reader.ReadString();
+	b := strings.Builder{}
+	for {
+		message, _ := reader.ReadString('\n')
+		b.Grow(len(total))
+		i := 0
+		for _, x := range message {
+			if unicode.IsLetter(x) {
+				switch i {
+				case 0:
+					b.WriteRune('h')
+					i++
+				case 1:
+					b.WriteRune('r')
+					i++
+				default:
+					b.WriteRune('u')
+				}
+			} else {
+				b.WriteRune(x)
+				i = 0
+			}
+		}
+		fmt.Print(b.String())
+		b.Reset()
+
+	}
+}
