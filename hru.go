9d779be49fd4915aaa7060ec7d9dbf1d2a052ac4
zemlyak <zemlyak@pandemonica.ru>
2021-12-12 15:16:33 +0300

    Hruuuuuuu hru hru

--- hru.go
+++ hru.go
@@ -2,39 +2,33 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
-	"strings"
 	"unicode"
 
 )
 
 func main() {
 	var reader = bufio.NewReader(os.Stdin)
-	b := strings.Builder{}
+	w := bufio.NewWriter(os.Stdout)
 	for {
 		message, _ := reader.ReadString('\n')
-		b.Grow(len(message))
 		i := 0
 		for _, x := range message {
 			if unicode.IsLetter(x) {
 				switch i {
 				case 0:
-					b.WriteRune('h')
+					w.WriteRune('h')
 					i++
 				case 1:
-					b.WriteRune('r')
+					w.WriteRune('r')
 					i++
 				default:
-					b.WriteRune('u')
+					w.WriteRune('u')
 				}
 			} else {
-				b.WriteRune(x)
+				w.WriteRune(x)
 				i = 0
 			}
 		}
-		fmt.Print(b.String())
-		b.Reset()
-
 	}
 }
